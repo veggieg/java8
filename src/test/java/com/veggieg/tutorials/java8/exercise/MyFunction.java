5b73270b48cb8214c26dde01920543c41ebf1d4a
veggieg <veggieg.zhu@gmail.com>
2018-07-20 00:47:53 +0800

    内置接口

---
+++ src/test/java/com/veggieg/tutorials/java8/exercise/MyFunction.java
@@ -0,0 +1,10 @@
+package com.veggieg.tutorials.java8.exercise;
+
+/**
+ * @author veggieg
+ * @since 2018-07-19 23:27
+ */
+@FunctionalInterface
+public interface MyFunction {
+    String getValue(String str);
+}
---
+++ src/test/java/com/veggieg/tutorials/java8/exercise/MyFunction2.java
@@ -0,0 +1,9 @@
+package com.veggieg.tutorials.java8.exercise;
+
+/**
+ * @author veggieg
+ * @since 2018-07-19 23:28
+ */
+public interface MyFunction2<T, R> {
+    R getValue(T t1, T t2);
+}
